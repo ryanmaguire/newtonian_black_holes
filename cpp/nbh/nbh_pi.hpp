b9e073b1fa55bac4558f9c91dca0388256899455
ryanmaguire <rmaguire@protonmail.com>
2023-09-20 11:26:14 -0400

    Adding multiples of pi.

---
+++ cpp/nbh/nbh_pi.hpp
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of newtonian_black_holes.                               *
+ *                                                                            *
+ *  newtonian_black_holes is free software: you can redistribute it and/or    *
+ *  modify it under the terms of the GNU General Public License as published  *
+ *  by the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  newtonian_black_holes is distributed in the hope that it will be useful   *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with newtonian_black_holes.  If not, see                            *
+ *  <https://www.gnu.org/licenses/>.                                          *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Provides various multiples of pi.                                     *
+ ******************************************************************************
+ *  Author: Ryan Maguire                                                      *
+ *  Date:   2023/09/20                                                        *
+ ******************************************************************************/
+
+/*  Include guard to prevent including this file twice.                       */
+#ifndef NBH_PI_HPP
+#define NBH_PI_HPP
+
+/*  The constants pi, 2 pi, and pi / 2, well beyond 64-bit double precision.  */
+#define NBH_PI (3.1415926535897932384626433832795028)
+#define NBH_TWO_PI (6.28318530717958647692528676655900559)
+#define NBH_HALF_PI (1.5707963267948966192313216916397514)
+
+#endif
+/*  End of include guard.                                                     */
