9354a24989ae35dd70a77373b17d7074fbee1d61
ryanmaguire <rmaguire@protonmail.com>
2023-09-24 18:35:10 -0400

    Adding license and docstring.

--- python/newtonian_black_hole.py
+++ python/newtonian_black_hole.py
@@ -1,11 +1,39 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of newtonian_black_holes.                                #
+#                                                                              #
+#   newtonian_black_holes is free software: you can redistribute it and/or     #
+#   modify it under the terms of the GNU General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   newtonian_black_holes is distributed in the hope that it will be useful,   #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with newtonian_black_holes.  If not, see                             #
+#   <https://www.gnu.org/licenses/>.                                           #
+################################################################################
+#   Purpose:                                                                   #
+#       Basic test of raytracing routines. Generates a single black hole.      #
+################################################################################
+#   Author:     Ryan Maguire                                                   #
+#   Date:       July 26, 2023.                                                 #
+################################################################################
+"""
 
-
+# All raytracing routines are provided here.
 import nbh
 
-name = "newtonian_black_hole.ppm"
+# Chosen parameters for the experiment.
+force = nbh.setup.gravity
+stopper = nbh.setup.stop
+colorer = nbh.color.checker_board
+solver = nbh.euler.path
 
-nbh.run.run(nbh.setup.gravity,
-            nbh.setup.stop,
-            nbh.color.checker_board,
-            nbh.euler.path,
-            name)
+# Run the raytracer.
+nbh.run.run(force, stopper, colorer, solver, "newtonian_black_hole.ppm")
