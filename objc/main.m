2fd0c88fc2274c96f27e406465d5f80481cfe403
ryanmaguire <rmaguire@protonmail.com>
2024-10-16 17:07:42 -0400

    Even more messing around in Objective-C.

--- objc/main.m
+++ objc/main.m
@@ -1,15 +1,27 @@
 #import "nbh/vec3.M"
 #import "nbh/vec6.M"
 
+void destroy(id *val)
+{
+    [*val release];
+    *val = nil;
+}
+
 int main(void)
 {
-    Vec3 *v = [[Vec3 alloc] init: 1.0 Y:2.0 Z:3.0];
-    Vec3 *u = [[Vec3 alloc] init: 4.0 Y:5.0 Z:6.0];
-    printf("||v|| = %E\n", [v norm]);
-    printf("v . u = %E\n", [v dot: u]);
-    [v plusEqual: u];
-    printf("v + u = <%e, %e, %e>\n", [v x], [v y], [v z]);
-    [v release];
-    [u release];
+    Vec3 *p = [[Vec3 alloc] init: 1.0 Y: 2.0 Z: 3.0];
+    Vec3 *v = [[Vec3 alloc] init: 4.0 Y: 5.0 Z: 6.0];
+    Vec6 *u = [[Vec6 alloc] init: p Vel: v];
+    Vec6 *w = [u copy];
+    Vec6 *sum = [u plus: w];
+
+    destroy(&p);
+    destroy(&v);
+    destroy(&u);
+    destroy(&w);
+
+    printf("%E\n", [sum norm]);
+    destroy(&sum);
+
     return 0;
 }
